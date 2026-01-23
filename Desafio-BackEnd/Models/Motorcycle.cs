08599a4b12d5785f800378c67adaed341513732f
Devdiego <devdiegosousa@gmail.com>
2026-01-22 22:24:40 -0300

    Feat: Start of the Motorcycle model

---
+++ Desafio-BackEnd/Models/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿namespace Desafio_BackEnd.Models
+{
+    public class Motorcycle
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Plate { get; set; }
+
+        public Motorcycle(int id, string model, int year, string plate)
+        {
+            Id = id;
+            Model = model;
+            Year = year;
+            Plate = plate;
+        }
+
+    }
+}
