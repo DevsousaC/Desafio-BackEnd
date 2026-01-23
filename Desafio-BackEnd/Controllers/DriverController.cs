52d72dec543b1eb6e3261e238aa5a34ba8c78f67
Devdiego <devdiegosousa@gmail.com>
2026-01-22 22:54:50 -0300

    Feat: Driver starter controller and model

---
+++ Desafio-BackEnd/Controllers/DriverController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("/entregadores")]
+    public class DriverController
+    {
+
+        [HttpPost]
+        public void SignupDriverController([FromBody] object driver)
+        {
+            // Implementation for signing up a driver
+        }
+
+        [HttpPost("{id}/cnh")]
+        public void SendDriverCnhController(int id)
+        {
+            // Implementation for sending driver's CNH
+        }
+    }
+}
---
+++ Desafio-BackEnd/Models/Driver.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Desafio_BackEnd.Models
+{
+    public class Driver
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cnpj { get; set; }
+        public DateTime Borned_In { get; set; }
+        public string Cnh_Number { get; set; }
+        public string Cnh_Image_Url { get; set; }
+
+        public Driver(int id, string name, string cnpj, DateTime borned_in, string cnh_number)
+        {
+            Id = id;
+            Name = name;
+            Cnpj = cnpj;
+            Borned_In = borned_in;
+            Cnh_Number = cnh_number;
+        }
+    }
+}
