55e755e6bae3b8fcb78c37421f8089df87412720
Devdiego <devdiegosousa@gmail.com>
2026-01-22 22:23:44 -0300

    Feat: Skeleton of MotoController
    MotoController use the route /motos

--- Desafio-BackEnd/Controllers/MotoController.cs
+++ Desafio-BackEnd/Controllers/MotoController.cs
@@ -1,6 +1,38 @@
-﻿namespace Desafio_BackEnd.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_BackEnd.Controllers
 {
+
+    [ApiController]
+    [Route("/motos")]
     public class MotoController
     {
+
+        [HttpPost]
+        public void SignupMoto([FromBody] Motorcycle moto)
+        {
+            // Implementation for signing up a motorcycle
+
+        }
+
+        [HttpGet("{id}")]
+        public void ReturnMotorcycleById(int id)
+        {
+            // Implementation for returning a motorcycle by ID
+        }
+
+        [HttpGet]
+        public void ReturnAllMotorcycles()
+        {
+            // Implementation for returning all motorcycles
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateMotorcyclePlate(int id, [FromBody] Motorcycle moto)
+        {
+            // Implementation for updating a motorcycle
+        }
+
+
     }
 }
